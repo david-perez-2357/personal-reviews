2876cbf9bbaa839fe083485461252a7259423534
David Perez <dperezfernandez@safareyes.es>
2025-03-26 21:41:53 +0100

    ♻️ Change equals to type strict mode

--- src/components/FooterTabBar.tsx
+++ src/components/FooterTabBar.tsx
@@ -35,22 +35,22 @@ const TabBar = memo(() => {
     return (
         <IonTabBar slot="bottom" className="flex bg-transparent justify-around min-h-24 border-t-2 border-t-primary">
             <IonTabButton rel="prefetch" tab={routes[0].name} href={routes[0].href} className="w-full h-full">
-                <div className={`flex flex-col justify-center items-center p-3 w-full h-full safe-area-padding-bottom ${routes[0].href == pathname ? "bg-tertiary text-tertiary-foreground" : "text-primary"}`}>
-                    <Star size={40} strokeWidth={routes[0].href == pathname ? 2 : 1} />
+                <div className={`flex flex-col justify-center items-center p-3 w-full h-full safe-area-padding-bottom ${routes[0].href === pathname ? "bg-tertiary text-tertiary-foreground" : "text-primary"}`}>
+                    <Star size={40} strokeWidth={routes[0].href === pathname ? 2 : 1} />
                     {t('reviews')}
                 </div>
             </IonTabButton>
 
             <IonTabButton rel="prefetch" tab={routes[1].name} href={routes[1].href} className="w-full h-full">
-                <div className={`flex flex-col justify-center items-center p-3 w-full h-full safe-area-padding-bottom ${routes[1].href == pathname ? "bg-tertiary text-tertiary-foreground" : "text-primary"}`}>
-                    <Box  size={40} strokeWidth={routes[1].href == pathname ? 2 : 1} />
+                <div className={`flex flex-col justify-center items-center p-3 w-full h-full safe-area-padding-bottom ${routes[1].href === pathname ? "bg-tertiary text-tertiary-foreground" : "text-primary"}`}>
+                    <Box  size={40} strokeWidth={routes[1].href === pathname ? 2 : 1} />
                     {t(routes[1].name)}
                 </div>
             </IonTabButton>
 
             <IonTabButton rel="prefetch" tab={routes[2].name} href={routes[2].href} className="w-full h-full">
-                <div className={`flex flex-col justify-center items-center p-3 w-full h-full safe-area-padding-bottom ${routes[2].href == pathname ? "bg-tertiary text-tertiary-foreground" : "text-primary"}`}>
-                    <Ellipsis  size={40} strokeWidth={routes[2].href == pathname ? 2 : 1} />
+                <div className={`flex flex-col justify-center items-center p-3 w-full h-full safe-area-padding-bottom ${routes[2].href === pathname ? "bg-tertiary text-tertiary-foreground" : "text-primary"}`}>
+                    <Ellipsis  size={40} strokeWidth={routes[2].href === pathname ? 2 : 1} />
                     {t(routes[2].name)}
                 </div>
             </IonTabButton>
