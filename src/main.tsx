eecdde0b8394eda3ed4c29ffeb15c39bda2de2b0
David Perez <dperezfernandez@safareyes.es>
2025-03-26 21:03:28 +0100

    🩹 Add i18next init

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './pages/App.tsx'
-import '../i18n.js';
+import i18n from '../i18n.ts';
+import {initReactI18next} from "react-i18next";
 
+i18n.use(initReactI18next);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
